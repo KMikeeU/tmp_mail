abbce3108855b20e8a37b4218ce19424147db678
Mikee <k.mike.u@gmail.com>
2018-11-17 19:56:32 +0100

    Added package information
    Added setup.py
    Modified .gitignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿from setuptools import setup
+
+setup(name='temp_mail',
+      version='0.1',
+      description='Unofficial Temp-Mail.org Client',
+      url='https://github.com/KMikeeU/temp_mail',
+      author='KMikeeU',
+      license='MIT',
+      packages=['temp_mail'],
+      zip_safe=False,
+      install_requires=[
+          'bs4'
+      ]
+      )
